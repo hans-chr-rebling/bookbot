c47c26b56d1e3624ee26b60a88f34636667d1da9
Hans Christian Rebling <hans.chr.rebling@gmail.com>
2024-11-13 19:37:12 +0100

    count_words function added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
-    print(read_file("frankenstein.txt"))
+    book_text = read_file("frankenstein.txt")
+    print(count_words(book_text))
 
 def read_file(file_name):
     file_path = "books/"
@@ -10,5 +11,8 @@ def read_file(file_name):
     except FileNotFoundError:
         print(f"ERROR: file:{file_name} not found!")    
     return file_contents
+
+def count_words(text):
+    return len(text.split())
 
 main()
