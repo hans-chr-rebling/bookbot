026653e07bbbab64b26b69f7677e07bac8848613
Hans Christian Rebling <hans.chr.rebling@gmail.com>
2024-11-14 14:01:16 +0100

    print a report

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 def main():
-    book_text = read_file("frankenstein.txt")
-    print(count_words(book_text))
+    print_report("books/frankenstein.txt")
 
-def read_file(file_name):
-    file_path = "books/"
+def read_file(file_path):
     file_contents = ""
     try:
-        with open(file_path+file_name) as file:
+        with open(file_path) as file:
             file_contents = file.read()
     except FileNotFoundError:
-        print(f"ERROR: file:{file_name} not found!")    
+        print(f"ERROR: file:{file_path} not found!")    
     return file_contents
 
-def count_words(text):
-    return len(text.split())
+def sort_on(dict):
+    return dict["num"]
+
+def print_report(file_path):
+    char_dict = {}
+    book_text = read_file(file_path)
+    words = book_text.split()
+    report = f"--- Begin report of {file_path} ---\n"
+    report += f"{len(words)} words found in the document\n\n"
+
+    for word in words:
+        for char in word:
+            char_lower_case = char.lower()
+            if char_lower_case.isalpha():
+                if char_lower_case not in char_dict:
+                    char_dict[char_lower_case] = 1
+                else:    
+                    char_dict[char_lower_case] += 1    
+    
+    results = [{"name": key, "num": value} for key, value in char_dict.items()]
+    results.sort(reverse=True, key=sort_on)
+
+    for result in results:
+        report += f"The '{result["name"]}' character was found {result["num"]} times\n"
+    report += "--- End report ---"
+
+    print(report)
+
 
 main()
