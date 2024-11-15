3459571453fedbe3fed6cad19b52fb660bcf02e2
Hans Christian Rebling <hans.chr.rebling@gmail.com>
2024-11-15 23:31:36 +0100

    properties and methods made private

--- file.py
+++ file.py
@@ -1,12 +1,12 @@
 class File:
      def __init__(self, file_path):
-         self.file_path = file_path
+         self.__file_path = file_path
          
      def read(self):
         file_contents = ""
         try:
-            with open(self.file_path) as file:
+            with open(self.__file_path) as file:
                 file_contents = file.read()
         except FileNotFoundError:
-            print(f"ERROR: file:{self.file_path} not found!")    
+            print(f"ERROR: file:{self.__file_path} not found!")    
         return file_contents
--- report.py
+++ report.py
@@ -2,17 +2,17 @@ from file import File
 
 class Report:
     def __init__(self, file_path):
-        self.file_path = file_path
-        self.file = File(file_path)
+        self.__file_path = file_path
+        self.__file = File(file_path)
 
-    def sort_on(self, dict):
+    def __sort_on(self, dict):
         return dict["num"]
 
     def print(self):
         char_dict = {}
-        book_text = self.file.read()
+        book_text = self.__file.read()
         words = book_text.split()
-        report = f"--- Begin report of {self.file_path} ---\n"
+        report = f"--- Begin report of {self.__file_path} ---\n"
         report += f"{len(words)} words found in the document\n\n"
 
         for word in words:
@@ -25,7 +25,7 @@ class Report:
                         char_dict[char_lower_case] += 1    
         
         results = [{"name": key, "num": value} for key, value in char_dict.items()]
-        results.sort(reverse=True, key=self.sort_on)
+        results.sort(reverse=True, key=self.__sort_on)
 
         for result in results:
             report += f"The '{result["name"]}' character was found {result["num"]} times\n"
